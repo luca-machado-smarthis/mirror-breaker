19d9d0e8cd4cf9be93a0c8a4d23085ad4afdff7e
Marcos Felipe Araújo <marcoz.araujoz@gmail.com>
2022-10-28 14:01:45 -0300

    Click do mouse inserido na main

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ import sys
 import pygame
 from level import Level
 from setting import *
+from menu import Menu
 
 class Game:
     def __init__(self):
         self.level = Level(level_data=level_map, surface=screen)
+        self.menu = Menu(screen)
     
     def run(self):
-        self.level.run()
+        self.menu.run(not(can_click))
 
 pygame.init()
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 game = Game()
+can_click = True
 
 #print(screen_height)
 
@@ -23,8 +26,10 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            click = True
+        if event.type == pygame.MOUSEBUTTONDOWN and can_click:
+            can_click = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            can_click = True
     screen.fill('black')
     game.run()
 
--- menu.py
+++ menu.py
@@ -5,9 +5,9 @@ class Menu:
     def __init__(self, surface):
         self.display_surface = surface
         self.buttons = pygame.sprite.Group()
-        self.buttons.add(Button('arquivo_fade.py','arquivo_full.py',(0,0)))#Jogar
-        self.buttons.add(Button('arquivo_fade.py','arquivo_full.py',(0,0)))#Level Select
-        self.buttons.add(Button('arquivo_fade.py','arquivo_full.py',(0,0)))#Sair
+        self.buttons.add(Button('arquivo_fade.py','arquivo_full.py',(0,0), ))#Jogar
+        #self.buttons.add(Button('arquivo_fade.py','arquivo_full.py',(0,0)))#Level Select
+        #self.buttons.add(Button('arquivo_fade.py','arquivo_full.py',(0,0)))#Sair
         
     
 
